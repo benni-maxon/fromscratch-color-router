49c5300daa78d2ab48f8f4bf3dc5d2a14be9a0a8
benni maxon <benni.maxon@gmail.com>
2023-01-24 12:54:59 -0800

    added default not found route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Main from './components/Main/Main.js';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import FourOhFour from './components/FourOhFour/FourOhFour.js';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/:r/:g/:b" component={Main} />
+        <Route exact path="/">
+          <Redirect to="/20/70/30" />
+        </Route>
+        <Route path="*">
+          <FourOhFour />
+        </Route>
       </Switch>
     </div>
   );
---
+++ src/components/FourOhFour/FourOhFour.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function FourOhFour() {
+  return (
+    <div>
+      <h2>404 NOT FOUND</h2>
+      <h2>🤪</h2>
+    </div>
+  );
+}
