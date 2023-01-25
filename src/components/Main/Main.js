07242bf2fda27829331e2d8fd4344c77d9fb1214
benni maxon <benni.maxon@gmail.com>
2023-01-24 16:13:00 -0800

    working on input and buttons to increment and decrement colors. red somewhat functional

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import { NavLink, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { NavLink, Redirect, Route, useHistory, useParams } from 'react-router-dom';
 import './Main.css';
 
 export default function Main() {
   const { r, g, b } = useParams();
+  const [red, setRed] = useState(r);
+  const [green, setGreen] = useState(g);
+  const [blue, setBlue] = useState(b);
+  let history = useHistory();
+  const handleIncrement = (e) => {
+    if (e.target.name === 'red') {
+      setRed((prev) => +prev + 1);
+      console.log('red', red);
+      history.replace(`/${red}/${green}/${blue}`);
+    }
+  };
+  const handleDecrement = (e) => {
+    if (e.target.name === 'red') {
+      setRed((prev) => +prev - 1);
+      console.log('red', red);
+    }
+  };
+  useEffect(() => {
+    const updatePage = () => history.replace(`/${red}/${green}/${blue}`);
+    updatePage();
+  }, [red, green, blue, history]);
+
   return (
     <main className="color-main" style={{ backgroundColor: `rgb(${r}, ${g}, ${b})` }}>
       <section className="color-links">
@@ -14,6 +36,15 @@ export default function Main() {
       <h2>
         rgb({r}, {g}, {b})
       </h2>
+      <div>
+        <button name="red" onClick={handleDecrement}>
+          -
+        </button>
+        <input value={red} />
+        <button name="red" onClick={handleIncrement}>
+          +
+        </button>
+      </div>
     </main>
   );
 }
